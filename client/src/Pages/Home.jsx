adba776193ee2031cfda30658a7c5c969590448d
Manasvi Pant <manasvipant1106@gmail.com>
2023-03-26 23:25:39 +0530

    Updated

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -6,7 +6,6 @@ import Categories from '../Components/Categories';
 import Products from '../Components/Products';
 import Newsletter from '../Components/Newsletter';
 import Footer from '../Components/Footer';
-
 const Home = () => {
   return (
     <div>
