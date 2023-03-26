b6d77b0e2461dcd616738bcd7e8d2fcca524e7ec
Manasvi Pant <manasvipant1106@gmail.com>
2023-03-27 00:28:33 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -20,11 +20,10 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/client/build/index.html"))
-  );
+app.use(express.static("client/build"));
+app.get("*", (req, res) =>{
+  res.sendFile(path.resolve(__dirname, "client","build","index.html"));
+});
 
 app.use(cors());
 app.use(express.json());
